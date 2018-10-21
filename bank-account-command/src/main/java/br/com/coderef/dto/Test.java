ad2bee5d79172bf5990469f0c24cf02febaeaafc
arsham <arsham.sedaghatbin@gmail.com>
2018-10-21 17:10:09 +0330

    query

---
+++ bank-account-command/src/main/java/br/com/coderef/dto/Test.java
@@ -0,0 +1,21 @@
+package br.com.coderef.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Test {
+
+    @Id
+    private String id;
+    private String test;
+}
